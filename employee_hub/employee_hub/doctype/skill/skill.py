849a920b88496291b0446e0865c196487c17715f
Raj <makwanaraj252@gmail.com>
2026-07-10 17:36:00 +0530

    Skill doctype created

--- employee_hub/employee_hub/doctype/skill/skill.py
+++ employee_hub/employee_hub/doctype/skill/skill.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2026, Raj and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Skill(Document):
-	pass
+	def validate(self):
+		self.skill_name = self.skill_name.strip()
+		self.validate_duplicate_skill()
+
+	def validate_duplicate_skill(self):
+		duplicate = frappe.db.sql(
+            """
+            SELECT name
+            FROM `tabSkill`
+            WHERE LOWER(skill_name)=LOWER(%s)
+            AND name!=%s
+            """,
+            (self.skill_name, self.name),
+        )
+
+		if duplicate:
+			frappe.throw("Skill Name already exists.")
